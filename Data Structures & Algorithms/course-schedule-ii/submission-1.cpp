23f7e5f73db8b743e2658c8fa61d98eccd9a0519
Aditya Goenka <aditya.goenka0207@gmail.com>
2026-09-19 16:42:59 +0530

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(V);
+
+        vector<int> indegree(V , 0);
+
+        vector<int> ans;
+
+        for(int i = 0;i<edges.size();i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            adj[v].push_back(u);
+            indegree[u]++;
+        }
+
+        queue<int> q;
+
+        for(int i = 0;i<V;i++){
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+
+        int count = 0;
+
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            count++;
+            ans.push_back(u);
+
+            for(int v : adj[u]){
+                indegree[v]--;
+
+                if(indegree[v] == 0)
+                    q.push(v);
+            }
+        }
+
+     return count == V ? ans : vector<int>{};
+    }
+};
